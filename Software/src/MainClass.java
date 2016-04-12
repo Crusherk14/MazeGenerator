b624c9b326234081089bddc546ac1f11e7e7643c
Mikhail <crusherk14@yandex.ru>
2016-04-12 23:48:51 +0300

    mainClass created

---
+++ Software/src/MainClass.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+
+
+public class MainClass {
+	public static void main(String[] args){
+		JFrame window = new JFrame();
+		window.setSize(640, 640);
+		window.setTitle("MazeGenerator v0.1a");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+	}
+}
