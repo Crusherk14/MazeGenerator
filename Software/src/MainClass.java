0a11237e9c016332bf7052bf1b88ef37d723cc95
Mikhail <crusherk14@yandex.ru>
2016-05-10 18:41:32 +0300

    Added: Basic Grid display with parameters

--- Software/src/MainClass.java
+++ Software/src/MainClass.java
@@ -6,37 +6,84 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JToggleButton;
+import javax.swing.border.TitledBorder;
 import javax.swing.JLabel;
+import javax.swing.JComponent;
 
 
 public class MainClass {
 	
-	public static int mazeSizeHeight = 30;
-	public static int mazeSizeWidth = 30;
+	public static int mazeSizeHeight = 10;
+	public static int mazeSizeWidth = 10;
+	
+	public final static int tileSize = 10;
 	
 	public static JFrame window = new JFrame();
 	public static JPanel panel=new JPanel();
-	public int[][] tilesArray = new int[mazeSizeWidth][mazeSizeHeight];
 	
 	
-	public static void main(String[] args){
+	public static void main(String[] args){		
 		window.setSize(640, 640);
-		window.setResizable(false);
+		//window.setResizable(false);
+		
 		window.setTitle("MazeGenerator v0.1a");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
 		
+		Box mainBox = Box.createHorizontalBox();
 		
-		panel.setLayout(new GridLayout(4,4));
+		Box topBox = Box.createVerticalBox();
+		topBox.setBorder(new TitledBorder("Parameters"));
+		mainBox.add(topBox);
 		
-		for (int i = 0; i<4*4; i++){
-		    JLabel label = new JLabel("Label "+i);
-		    panel.add(label);
-		}
-		window.add(panel);
+		Grid grid = new Grid();
+		Box box_grid = Box.createVerticalBox();
+		box_grid.setBorder(new TitledBorder("Maze display"));
+		box_grid.add(grid);
+		//window.add(box_grid);
+		mainBox.add(box_grid);
 		
+		window.setContentPane(mainBox);
+		
+		grid.fillCell(2, 1);	//X;Y coordinates starting from 0;0
 	}
+	
+	//Grid
+	public static class Grid extends JPanel {
+
+        private List<Point> fillCells;
+
+        public Grid() {
+            fillCells = new ArrayList<>();
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            for (Point fillCell : fillCells) {
+                int cellX = 10 + (fillCell.x * 10);
+                int cellY = 10 + (fillCell.y * 10);
+                g.setColor(Color.RED);
+                g.fillRect(cellX, cellY, 10, 10);
+            }
+            g.setColor(Color.BLACK);
+            g.drawRect (MainClass.tileSize, MainClass.tileSize, MainClass.mazeSizeWidth*MainClass.tileSize, MainClass.mazeSizeHeight*MainClass.tileSize);
+
+            for (int i = MainClass.tileSize; i <= MainClass.mazeSizeWidth*MainClass.tileSize+MainClass.tileSize; i+= MainClass.tileSize)
+                g.drawLine (i, MainClass.tileSize, i, (1+MainClass.mazeSizeHeight)*MainClass.tileSize);
+
+            for (int i = 10; i <= MainClass.mazeSizeHeight*MainClass.tileSize+MainClass.tileSize; i+= MainClass.tileSize)
+                g.drawLine (MainClass.tileSize, i, (1+MainClass.mazeSizeWidth)*MainClass.tileSize, i);
+        }
+
+        public void fillCell(int x, int y) {
+            fillCells.add(new Point(x, y));
+            repaint();
+        }
+
+    }
 }
-//testtest
