16d4989dca62cdb265b451c645cf4bc61647d799
Mikhail <crusherk14@yandex.ru>
2016-05-18 22:06:42 +0300

    Changed: TileClass
    Added ID,coordinate[2],paths variables

--- Software/src/TileClass.java
+++ Software/src/TileClass.java
@@ -1,9 +1,17 @@
+import java.util.ArrayList;
+
 public class TileClass {
 	
-	private String state="empty";
+	private int ID;	//Unique ID
+	private int coordinate[] = new int[2];	//Y;X coordinates
+	private String state;	//empty/wall/start/finish
+	private ArrayList<Integer> paths = new ArrayList<Integer>(4);	//Assigned paths
 	
-	public TileClass(){
-		//state = "empty";
+	public TileClass(int ID,int posY,int posX){
+		this.ID = ID;
+		this.coordinate[0]=posY;
+		this.coordinate[1]=posX;
+		this.state = "empty";
 	}
 	
 	public String getState(){
@@ -13,5 +21,12 @@ public class TileClass {
 	public void setState(String state){
 		this.state = state;
 	}
-
+	
+	public void assignPath(int pathID){
+		this.paths.add(pathID);
+	}
+	
+	public int[] getCoords(){
+		return coordinate;
+	}
 }
